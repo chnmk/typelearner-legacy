9e45ffd73f4566e03f004ef059cf966e0c0e7fc8
chnmk <a2molchanov@ya.ru>
2023-11-17 23:31:16 +0300

    ops: add vue.config.js and deploy.sh for GitHub

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 //Hello, GitHub!
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/typelearner/'
+    : '/'
 })
